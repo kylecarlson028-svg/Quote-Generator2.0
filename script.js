c1d6188cd4ffbf073571b4e7ac4ba5957a821fab
kylecarlson028-svg <kylecarlson028@gmail.com>
2025-10-05 15:34:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const quotes = [
   { text: "The only thing we have to fear is fear itself.", author: "Franklin D. Roosevelt", topic: "motivation" },
   { text: "Never think that war, no matter how necessary, nor how justified, is not a crime.", author: "Ernest Hemingway", topic: "WW1" }
 ];
-
+console.log("Script loaded!");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const newQuoteButton = document.getElementById("new-quote");
