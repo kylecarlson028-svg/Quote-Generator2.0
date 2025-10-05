dd16241ae935a303411e279214a79bd6219f866e
kylecarlson028-svg <kylecarlson028@gmail.com>
2025-10-05 15:29:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const quotes = [
-  const quotes = [
   { text: "In the midst of chaos, there is also opportunity.", author: "Sun Tzu", topic: "war" },
   { text: "The only thing we have to fear is fear itself.", author: "Franklin D. Roosevelt", topic: "motivation" },
   { text: "Never think that war, no matter how necessary, nor how justified, is not a crime.", author: "Ernest Hemingway", topic: "WW1" }
@@ -14,4 +13,3 @@ newQuoteButton.addEventListener("click", () => {
   quoteText.textContent = `"${random.text}"`;
   authorText.textContent = `— ${random.author}`;
 });
-
